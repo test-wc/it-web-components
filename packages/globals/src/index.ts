50197f144d40eeb16dae669d59837ebd0a07c7fe
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-05-21 16:33:31 +0200

    fix: fixes

--- packages/globals/src/index.ts
+++ packages/globals/src/index.ts
@@ -1,2 +1,4 @@
 export { BaseComponent } from './base-component/base-component.js';
 export TrackFocus from './utils/track-focus.js';
+export FormMixin from './mixins/form.js';
+export {ValidityMixin, VALIDATION_STATUS} from './mixins/validity.js';
