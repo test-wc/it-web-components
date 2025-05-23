ccf66ce919affcef145d34d8b1be16af47b3a927
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-05-23 12:21:11 +0200

    fix: globals

--- packages/globals/src/index.ts
+++ packages/globals/src/index.ts
@@ -1,7 +1,7 @@
 import TrackFocus from './utils/track-focus.js';
 import FormMixin from './mixins/form.js';
+import ValidityMixin from './mixins/validity.js';
 
+export { TrackFocus, FormMixin, ValidityMixin };
 export { BaseComponent } from './base-component/base-component.js';
-export { ValidityMixin, VALIDATION_STATUS } from './mixins/validity.js';
-
-export { TrackFocus, FormMixin };
+export { VALIDATION_STATUS } from './mixins/validity.js';
