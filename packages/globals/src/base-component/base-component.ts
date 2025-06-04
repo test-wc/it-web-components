3a139a5ba3e9f956485d2f6427ca12f7840b4103
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-06-04 12:34:46 +0200

    chore: commented trackfocus

--- packages/globals/src/base-component/base-component.ts
+++ packages/globals/src/base-component/base-component.ts
@@ -1,5 +1,5 @@
 import { LitElement, unsafeCSS, CSSResult } from 'lit';
-import TrackFocus from '../utils/track-focus.js';
+// import TrackFocus from '../utils/track-focus.js';
 
 // // Tipo costruttore generico che restituisce una sottoclasse di LitElement
 // type LitElementConstructor = new (...args: any[]) => LitElement;
@@ -9,20 +9,19 @@ import TrackFocus from '../utils/track-focus.js';
  * con stili personalizzati.
  */
 
-export const BaseComponent = (style: string | CSSResult): typeof LitElement => {
-  return class BaseComponentInternal extends LitElement {
+export const BaseComponent = (style: string | CSSResult): typeof LitElement =>
+  class BaseComponentInternal extends LitElement {
     static override styles = [unsafeCSS(style)];
 
-    protected static addFocus(element: HTMLElement) {
-      new TrackFocus(element);
+    protected static addFocus(/* element: HTMLElement */) {
+      // new TrackFocus(element); //per il momento è stato disattivato perchè ci sono le pseudo classi ::focus-visible per fare quello che fa TrackFocus. Si possono aggiungere regole css in bsi-italia 3 dato che stiamo facendo una breaking release di bsi.
     }
 
     protected static composeClass(...classes: any) {
       let composedClass = '';
       classes.forEach((newClass: string) => {
-        composedClass += ' ' + newClass;
+        composedClass += ` ${newClass}`;
       });
       return composedClass.trim();
     }
   };
-};
