ed7afbeefe423074d71a7e01846232d5b360af09
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-06-04 12:05:59 +0200

    fix: dependencies

--- packages/globals/src/base-component/base-component.ts
+++ packages/globals/src/base-component/base-component.ts
@@ -1,15 +1,23 @@
-import { LitElement, unsafeCSS } from 'lit';
-import { TrackFocus } from '@it-web-components/globals';
+import { LitElement, unsafeCSS, CSSResult } from 'lit';
+import TrackFocus from '../utils/track-focus.js';
 
-export const BaseComponent = (style: any): typeof LitElement => {
+// // Tipo costruttore generico che restituisce una sottoclasse di LitElement
+// type LitElementConstructor = new (...args: any[]) => LitElement;
+
+/**
+ * Factory function per creare una base class estendibile
+ * con stili personalizzati.
+ */
+
+export const BaseComponent = (style: string | CSSResult): typeof LitElement => {
   return class BaseComponentInternal extends LitElement {
     static override styles = [unsafeCSS(style)];
 
-    protected addFocus(element: HTMLElement) {
+    protected static addFocus(element: HTMLElement) {
       new TrackFocus(element);
     }
 
-    protected composeClass(...classes: any) {
+    protected static composeClass(...classes: any) {
       let composedClass = '';
       classes.forEach((newClass: string) => {
         composedClass += ' ' + newClass;
---
+++ packages/it-tabs/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ItTabs = void 0;
+var it_tabs_js_1 = require("./it-tabs.js");
+Object.defineProperty(exports, "ItTabs", { enumerable: true, get: function () { return it_tabs_js_1.ItTabs; } });
