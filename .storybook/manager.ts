131d32d35617d7d405368776a8bfd4cc630e3741
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-06-18 10:23:32 +0200

    fix: brandUrl in storybook

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.light, // puoi anche usare themes.dark o un tema personalizzato
     brandTitle: 'Design Web Components',
-    brandUrl: 'https://tuosito.it', // opzionale
+    brandUrl: 'https://github.com/italia/design-web-components', // opzionale
     brandImage: './logo.png',
   },
 });
