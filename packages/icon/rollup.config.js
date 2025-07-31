d1034ab4587722a9a5d05b8a764da25da3f4dfda
Martina Bustacchini <martina.bustacchini@redturtle.it>
2025-07-31 16:32:39 +0200

    fix: trying to fix vercel deployment

--- packages/icon/rollup.config.js
+++ packages/icon/rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { litScss } from 'rollup-plugin-scss-lit';
+import copy from 'rollup-plugin-copy';
 /**
  * @type {import('rollup').RollupOptions}
  */
@@ -24,6 +25,10 @@ export default {
         loadPaths: ['node_modules'],
         silenceDeprecations: ['import'],
       },
+    }),
+    copy({
+      targets: [{ src: 'src/icon-registry.ts', dest: 'dist/src' }],
+      hook: 'writeBundle',
     }),
   ],
   external: [/^lit(\/|$)/],
